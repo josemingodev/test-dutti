d248c08c024d22cf8eb58bbca10cd47ff24685db
DESKTOP-DUQO754\mingo <jose.mingo02@gmail.com>
2020-10-26 14:10:34 +0100

    He añadido la nueva ruta starships.

--- src/app/views/auth/auth.views.routing.ts
+++ src/app/views/auth/auth.views.routing.ts
@@ -1,5 +1,6 @@
 import {AuthGuard} from '../../../modules/auth/auth.guard';
 import {HomeComponent} from './home/home.component';
+import {StarshipsSectionComponent} from './starships-section/starships-section.component';
 
 
 export class AuthViewsRouting {
@@ -8,6 +9,7 @@ export class AuthViewsRouting {
     children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
       {path: 'home', component: HomeComponent},
+      {path: 'starships', component: StarshipsSectionComponent},
     ]
   };
 }

2ce2e55b9f323758fb8a08e06329a1f30ae04473
DESKTOP-DUQO754\mingo <jose.mingo02@gmail.com>
2020-10-26 13:33:27 +0100

    He creado el modulo starship para agrupar todos sus componentes.

---
+++ src/app/components/starship/starship.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MaterialModule} from '../../material.module';
+import {FlexModule} from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexModule
+  ],
+  exports: []
+})
+export class StarshipModule { }

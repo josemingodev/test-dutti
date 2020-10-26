e06c9dcd087424b85115d8c9a3d99b59915fdbc6
DESKTOP-DUQO754\mingo <jose.mingo02@gmail.com>
2020-10-26 12:53:37 +0100

    He añadido las AuthViewsRouting al modulo principal.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NoAuthViewsRouting} from './views/no-auth/no-auth.views.routing';
+import {AuthViewsRouting} from './views/auth/auth.views.routing';
 
 const routes: Routes = [
   NoAuthViewsRouting.ROUTES,
+  AuthViewsRouting.ROUTES,
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: '**', redirectTo: '/login'},
 ];

08abd60321ae04e88927d8c42f525b752f1e64c1
DESKTOP-DUQO754\mingo <jose.mingo02@gmail.com>
2020-10-26 15:09:01 +0100

    He añadido un logout automatico cuando se cierra la web.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {AuthService} from '../modules/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'test-dutti';
+  constructor(private authService: AuthService) {
+  }
+  ngOnDestroy() {
+    this.authService.logout();
+  }
 }

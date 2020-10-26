ac455c7650953fb91d15555823362c4863189613
DESKTOP-DUQO754\mingo <jose.mingo02@gmail.com>
2020-10-26 13:17:58 +0100

    He añadido el servicio AuthService al componente toolbar para hacer logout.

--- src/app/components/common/layouts/toolbar/toolbar.component.ts
+++ src/app/components/common/layouts/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MatSidenav} from '@angular/material';
+import {AuthService} from '../../../../../modules/auth/auth.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,9 +11,14 @@ export class ToolbarComponent implements OnInit {
   @Input() sectionActive: string;
   @Input() sidenav: MatSidenav;
   @Input() activateMenu: boolean;
-  constructor() {
+
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
